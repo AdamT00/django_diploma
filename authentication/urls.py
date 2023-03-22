247f2171f57ecafecf6a3b9971c52a3ad75dd0e0
Adam Tolcser <tolcseradam@gmail.com>
2023-03-22 15:44:53 +0100

    Removed unneeded lines.

--- authentication/urls.py
+++ authentication/urls.py
@@ -3,6 +3,4 @@ from . import views
 
 urlpatterns = [
     path('', views.AuthView.as_view(), name='hello_auth'),
-    path('users/', views.AuthView.as_view(), name='users'),
-    # path('?name=<str:name>/?password=<str:password>', views.AuthView.as_view(), name='user_authentication'),
 ]
--- authentication/views.py
+++ authentication/views.py
@@ -2,10 +2,3 @@ from django.shortcuts import render
 from rest_framework import generics, status
 from rest_framework.response import Response
 
-
-class AuthView(generics.GenericAPIView):
-    def authenticate_user(self):
-        return True
-
-    def get(self, request):
-        return Response(data={'message': 'Hello Auth', 'name': 'asd'}, status=status.HTTP_200_OK)
