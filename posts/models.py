15896d50d928be841cab01ab3bc38fe552d239e7
Adam Tolcser <tolcseradam@gmail.com>
2023-03-24 11:45:10 +0100

    Added migrations, and database fixes.

--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
-    body = models.TextField
-    date_created = models.DateTimeField
-    date_updated = models.DateTimeField
+    body = models.TextField(default='')
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
