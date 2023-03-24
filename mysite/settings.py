b6917f3986f2d68c9f334d6130d22aedf591acd5
Adam Tolcser <tolcseradam@gmail.com>
2023-03-24 11:21:58 +0100

    Fixed timezone and model fields

--- mysite/settings.py
+++ mysite/settings.py
@@ -87,7 +87,7 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 LANGUAGE_CODE = 'en-us'
-TIME_ZONE = 'Europe/Belgrade'
+TIME_ZONE = 'UTC'
 USE_I18N = True
 USE_TZ = True
 STATIC_URL = 'static/'
--- posts/models.py
+++ posts/models.py
@@ -3,4 +3,6 @@ from django.db import models
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
-    body = models.CharField(max_length=255)
+    body = models.TextField
+    date_created = models.DateTimeField
+    date_updated = models.DateTimeField
