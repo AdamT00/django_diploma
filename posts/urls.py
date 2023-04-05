8c90849848f45ba726a97491e485527173cdd5dc
Adam Tolcser <tolcseradam@gmail.com>
2023-04-05 13:00:06 +0200

    Url fix

--- posts/urls.py
+++ posts/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.Posts.as_view(), name='posts'),
     path('<int:id>/', views.PostById.as_view(), name='post_by_id'),
-    re_path('post/(?P<id>-*[0-9]+)/\\Z', views.PostById.as_view(), name='post_by_id'),
+    re_path('(?P<id>-*[0-9]+)/\\Z', views.PostById.as_view(), name='post_by_id'),
 ]
