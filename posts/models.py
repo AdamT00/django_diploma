78f51a47ba7701884349f42c248391a7bc947976
Adam Tolcser <tolcseradam@gmail.com>
2023-07-24 14:15:18 +0200

    Created new column in the comments table, date_updated.

--- posts/models.py
+++ posts/models.py
@@ -18,6 +18,7 @@ class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     date_created = models.DateTimeField(auto_now_add=True)
+    date_updated = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.text
