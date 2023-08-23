3bf1e478797150a1b6147ded9878f6c59c81821a
Adam Tolcser <tolcserxd@gmail.com>
2023-08-23 16:26:43 +0200

    Created search and filter features on the admin page.

--- posts/admin.py
+++ posts/admin.py
@@ -2,12 +2,57 @@ from django.contrib import admin
 from .models import Post, Comment
 
 
+class DecadeBornListFilter(admin.SimpleListFilter):
+    # Human-readable title which will be displayed in the
+    # right admin sidebar just above the filter options.
+    title = "decade born"
+
+    # Parameter for the filter that will be used in the URL query.
+    parameter_name = "decade"
+
+    def lookups(self, request, model_admin):
+        """
+        Returns a list of tuples. The first element in each
+        tuple is the coded value for the option that will
+        appear in the URL query. The second element is the
+        human-readable name for the option that will appear
+        in the right sidebar.
+        """
+        return [
+            ("80s", "in the eighties"),
+            ("90s", "in the nineties"),
+        ]
+
+    def queryset(self, request, queryset):
+        """
+        Returns the filtered queryset based on the value
+        provided in the query string and retrievable via
+        `self.value()`.
+        """
+        # Compare the requested value (either '80s' or '90s')
+        # to decide how to filter the queryset.
+        if self.value() == "80s":
+            return queryset.filter(
+                birthday__gte=date(1980, 1, 1),
+                birthday__lte=date(1989, 12, 31),
+            )
+        if self.value() == "90s":
+            return queryset.filter(
+                birthday__gte=date(1990, 1, 1),
+                birthday__lte=date(1999, 12, 31),
+            )
+
+
 class PostAdmin(admin.ModelAdmin):
-    fields = ['title', 'body', 'user']
+    list_display = ['title', 'user']
+    list_filter = ['user']
+    search_fields = ['title', 'user__username']
 
 
 class CommentAdmin(admin.ModelAdmin):
-    fields = ['text', 'post', 'user']
+    list_display = ['text', 'post', 'user']
+    list_filter = ['post', 'user']
+    search_fields = ['text', 'post__title', 'user__username']
 
 
 admin.site.register(Post, PostAdmin)
